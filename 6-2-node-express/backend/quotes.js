a6d33278dadaac93a066bf6b1105cb77e18676e7
UHasanU <mualimha@gmail.com>
2025-11-12 01:12:47 +0300

    Finialize Lab

--- 6-2-node-express/backend/quotes.js
+++ 6-2-node-express/backend/quotes.js
@@ -8,5 +8,6 @@ const quotes = [
 ];
 
 export function getRandomQuote() {
- return {/*write code to here*/}
+   const idx = getRandomInt(quotes.length);
+  return quotes[idx];
 }
--- 6-2-node-express/backend/server.js
+++ 6-2-node-express/backend/server.js
@@ -2,10 +2,17 @@ import express from "express";
 import cors from "cors";
 import { getRandomQuote } from "./quotes.js";
 
-{/*write code for cors*/}
+const app = express();
+app.use(cors());
 
 
-{/*write code to define routes*/}
+app.get('/api/quote', (req, res) => {
+  const quote = getRandomQuote();
+  res.json({ quote });
+});
 
 
-{/*write code to create server*/}
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
